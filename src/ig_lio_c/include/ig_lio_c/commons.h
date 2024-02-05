96d1cc2ad82363e1685fe5807bd4e914b2fd350d
INIF-FISH <iratecat1@outlook.com>
2024-02-05 14:30:32 +0800

    update

---
+++ src/ig_lio_c/include/ig_lio_c/commons.h
@@ -0,0 +1,25 @@
+#ifndef _COMMONS_H
+#define _COMMONS_H
+
+#include <mutex>
+#include <vector>
+#include <queue>
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <livox_ros_driver2/msg/custom_msg.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+namespace IG_LIO
+{
+    
+} // namespace IG_LIO
+
+
+#endif // _COMMONS_H
---
+++ src/ig_lio_c/include/ig_lio_c/localizer/icp_localizer.h
@@ -0,0 +1,18 @@
+#ifndef _ICP_LOCALIZER_H
+#define _ICP_LOCALIZER_H
+
+#include <string>
+#include "commons.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/registration/icp.h>
+#include <pcl/features/normal_3d.h>
+
+namespace IG_LIO
+{
+    
+} // namespace IG_LIO
+
+
+#endif // _ICP_LOCALIZER_H
