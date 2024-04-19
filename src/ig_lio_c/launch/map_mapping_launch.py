8eea4dc3dc9fab835f137f77e7ee3283d23421a3
INIF-FISH <iratecat1@outlook.com>
2024-04-19 20:03:05 +0800

    disable imu_complementary_filter_node

--- src/ig_lio_c/launch/map_mapping_launch.py
+++ src/ig_lio_c/launch/map_mapping_launch.py
@@ -42,19 +42,19 @@ def generate_launch_description():
         parameters=[configured_params],
     )
 
-    imu_complementary_filter_node = Node(
-                package='imu_complementary_filter',
-                executable='complementary_filter_node',
-                name='complementary_filter_gain_node',
-                output='screen',
-                parameters=[
-                    {'do_bias_estimation': True},
-                    {'do_adaptive_gain': True},
-                    {'use_mag': False},
-                    {'gain_acc': 0.01},
-                    {'gain_mag': 0.01},
-                ],
-            )
+    # imu_complementary_filter_node = Node(
+    #             package='imu_complementary_filter',
+    #             executable='complementary_filter_node',
+    #             name='complementary_filter_gain_node',
+    #             output='screen',
+    #             parameters=[
+    #                 {'do_bias_estimation': True},
+    #                 {'do_adaptive_gain': True},
+    #                 {'use_mag': False},
+    #                 {'gain_acc': 0.01},
+    #                 {'gain_mag': 0.01},
+    #             ],
+    #         )
 
     ld = LaunchDescription([DeclareLaunchArgument('namespace', default_value='',
                             description='Top-level namespace'),
